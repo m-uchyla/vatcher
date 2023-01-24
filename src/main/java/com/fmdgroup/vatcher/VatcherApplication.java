c303422466278e9561449ea48ad1f4ff00d133ae
mateusz uchyla <mateusz.uchyla@fdmgroup.com>
2023-01-24 14:55:05 +0100

    init

---
+++ src/main/java/com/fmdgroup/vatcher/VatcherApplication.java
@@ -0,0 +1,13 @@
+package com.fmdgroup.vatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmdgroup/vatcher/VatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmdgroup.vatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
