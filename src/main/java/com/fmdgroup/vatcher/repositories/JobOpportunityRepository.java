f419abb29b1846afe9f29a762cbfe1417706b7df
VladParfan <vladparfan1997@gmail.com>
2023-01-25 15:00:52 +0100

    Model: JobOpportunity Repository: JobOpportunity

---
+++ src/main/java/com/fmdgroup/vatcher/repositories/JobOpportunityRepository.java
@@ -0,0 +1,7 @@
+package com.fmdgroup.vatcher.repositories;
+import org.springframework.data.repository.CrudRepository;
+import com.fmdgroup.vatcher.model.JobOpportunity;
+
+public interface JobOpportunityRepository extends CrudRepository<JobOpportunity ,Long> {
+
+}
