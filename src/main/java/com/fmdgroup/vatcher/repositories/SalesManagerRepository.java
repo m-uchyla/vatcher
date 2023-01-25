38c29ebce055340173e7234b26c0432ec721b61a
Paleolama <nicole.misztak@gmail.com>
2023-01-25 15:30:14 +0100

    Repository

---
+++ src/main/java/com/fmdgroup/vatcher/repositories/SalesManagerRepository.java
@@ -0,0 +1,7 @@
+package com.fmdgroup.vatcher.repositories;
+import org.springframework.data.repository.CrudRepository;
+import com.fmdgroup.vatcher.model.SalesManager;
+
+public interface SalesManagerRepository extends CrudRepository<SalesManager, Long>{
+
+}
