79b40da228ec524a47e332ed3722f9fc3117fec5
FilipDabrowski <fil.dabr@gmail.com>
2023-01-26 15:35:30 +0100

    Update UserController.java

--- src/main/java/com/fmdgroup/vatcher/controllers/UserController.java
+++ src/main/java/com/fmdgroup/vatcher/controllers/UserController.java
@@ -4,40 +4,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.fdmgroup.PlacesDemo.services.IPlaceService;
+import com.fmdgroup.vatcher.model.SingleUser;
 import com.fmdgroup.vatcher.repositories.UserRepository;
 
 @Controller
 public class UserController {
-	private final UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
+	//private final UserRepository userRepository;
 
 	public UserController(UserRepository userRepository) {
-	super();
-	this.userRepository = userRepository;
-}
-	
-	
+		super();
+		this.userRepository = userRepository;
+	}
+
 	@RequestMapping("/users")
 	public String getUsers(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "singleUser";
 	}
-	
-	
-	@RequestMapping(method=RequestMethod.GET, value="/addUser")
-	@GetMapping(value="/addUser")
+//GetMapping allows user to open a website with places with input a data. 
+	@RequestMapping(method = RequestMethod.GET, value = "/addUser")
+	@GetMapping(value = "/addUser")
 	public String goToIndex(ModelMap model) {
-		
+
 		return "addUser";
 	}
 
+	@PostMapping(value = "/addUser")
+	public String createNewUser(ModelMap model, @ModelAttribute SingleUser user) {
+		
+		userRepository.save( user);
+		return "redirect:/singleUser";
 
+	}
 
 }
