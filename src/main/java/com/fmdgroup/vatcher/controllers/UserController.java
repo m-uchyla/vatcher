905707c186fcb2f54e5bb90d53cde1d369634551
FilipDabrowski <fil.dabr@gmail.com>
2023-01-26 15:12:13 +0100

    Update UserController.java

--- src/main/java/com/fmdgroup/vatcher/controllers/UserController.java
+++ src/main/java/com/fmdgroup/vatcher/controllers/UserController.java
@@ -1,8 +1,16 @@
 package com.fmdgroup.vatcher.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.fmdgroup.vatcher.repositories.UserRepository;
 
 @Controller
@@ -20,6 +28,14 @@ public class UserController {
 	public String getUsers(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "singleUser";
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.GET, value="/addUser")
+	@GetMapping(value="/addUser")
+	public String goToIndex(ModelMap model) {
+		
+		return "addUser";
 	}
 
 
