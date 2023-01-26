5776ef33dbc8c9946ef3c571d15a5b1a828281b7
M. Uchyła <czarnytroll@gmail.com>
2023-01-26 15:47:51 +0100

    Update UserController.java

--- src/main/java/com/fmdgroup/vatcher/controllers/UserController.java
+++ src/main/java/com/fmdgroup/vatcher/controllers/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.fdmgroup.PlacesDemo.services.IPlaceService;
 import com.fmdgroup.vatcher.model.SingleUser;
 import com.fmdgroup.vatcher.repositories.UserRepository;
 
