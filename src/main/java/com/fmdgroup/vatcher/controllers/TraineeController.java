b1381653ab31cf137fec63366e927418647e0265
FilipDabrowski <fil.dabr@gmail.com>
2023-01-27 09:07:04 +0100

    Update TraineeController.java

--- src/main/java/com/fmdgroup/vatcher/controllers/TraineeController.java
+++ src/main/java/com/fmdgroup/vatcher/controllers/TraineeController.java
@@ -1,31 +1,47 @@
 package com.fmdgroup.vatcher.controllers;
+
+import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fmdgroup.vatcher.model.Trainee;
 import com.fmdgroup.vatcher.repositories.TraineeRepository;
 import com.fmdgroup.vatcher.repositories.UserRepository;
 
 @Controller
 public class TraineeController {
-	
+
 	private final TraineeRepository traineeRepository;
-	
 
 	public TraineeController(TraineeRepository traineeRepository, UserRepository userRepository) {
 		super();
-		this.traineeRepository = traineeRepository;	
+		this.traineeRepository = traineeRepository;
 	}
-	
-	
+
 	@RequestMapping("/trainees")
-	public String getTrainees(Model model) {	
+	public String getTrainees(Model model) {
 		model.addAttribute("trainees", traineeRepository.findAll());
 		return "trainee";
 	}
-	
-	//HOW TO
-	//Add an attribute called "books" to a model object, sets its value to the result of 
-	//calling the "findAll()" method on a "bookRepository" object. 
-	//This retrieves all the books from a database, 
-	//and makes them available to the view or template being rendered by the application.
+
+	@RequestMapping("/addQualification")
+	public Set<String> addQualification(Long id, String qualification) throws Exception {
+
+		Optional<Trainee> trainee = traineeRepository.findById(id);
+		if (trainee.isPresent()) {
+			Set<String> traineeQualifications = trainee.get().getQualifications();
+			traineeQualifications.add(qualification);
+			traineeRepository.save(trainee.get());
+
+			return trainee.get().getJobsPreferences();
+		} else {
+			return trainee.get().getJobsPreferences();
+		}
+
+	}
+
 }
