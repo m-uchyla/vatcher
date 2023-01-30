13edf952d92416c0543fa5c3b74d4e74e7397240
FilipDabrowski <fil.dabr@gmail.com>
2023-01-30 18:44:00 +0100

    Create TraineeService.java

---
+++ src/main/java/com/fmdgroup/vatcher/services/TraineeService.java
@@ -0,0 +1,53 @@
+package com.fmdgroup.vatcher.services;
+
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.fmdgroup.vatcher.model.Trainee;
+import com.fmdgroup.vatcher.repositories.TraineeRepository;
+@Service
+public class TraineeService implements ITraineeService {
+
+private TraineeRepository traineeRepo;
+
+	@Override
+	public Set<String> traineeQualification() {
+	
+		return null;
+	}
+
+	
+
+	@Override
+	public Trainee findTraineeById(Long id) throws Exception {
+		Optional<Trainee> optTrainee = traineeRepo.findById(id);
+		return optTrainee.orElseThrow(()-> new Exception());
+	}
+
+	@Override
+	public void updateQualification(String qualification, Long id) throws Exception {
+		Trainee trainee = findTraineeById(id);
+		Set<String> traineeQualification = trainee.getQualifications();
+		traineeQualification.add(qualification);
+		traineeRepo.save(trainee);
+		
+	}
+
+
+
+	
+
+
+	@Override
+	public String addTrainee(Trainee trainee) {
+		traineeRepo.save(trainee);
+		return trainee.getUser().getName();
+	}
+
+
+
+	
+	
+}
