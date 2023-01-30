cb95e886b886b9d386c831610fa558deb72fef34
FilipDabrowski <fil.dabr@gmail.com>
2023-01-30 18:44:13 +0100

    Update TraineeRepository.java

--- src/main/java/com/fmdgroup/vatcher/repositories/TraineeRepository.java
+++ src/main/java/com/fmdgroup/vatcher/repositories/TraineeRepository.java
@@ -1,9 +1,12 @@
 package com.fmdgroup.vatcher.repositories;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fmdgroup.vatcher.model.Trainee;
 
 public interface TraineeRepository extends JpaRepository<Trainee, Long> {
-
+	
+	
 }
