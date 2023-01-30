bfde70c372855443e177d1d2b6ac8bff30c0eeb4
FilipDabrowski <fil.dabr@gmail.com>
2023-01-30 18:43:11 +0100

    Update TraineeController.java

--- src/main/java/com/fmdgroup/vatcher/controllers/TraineeController.java
+++ src/main/java/com/fmdgroup/vatcher/controllers/TraineeController.java
@@ -3,45 +3,66 @@ package com.fmdgroup.vatcher.controllers;
 import java.util.Optional;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fmdgroup.vatcher.model.SingleUser;
 import com.fmdgroup.vatcher.model.Trainee;
 import com.fmdgroup.vatcher.repositories.TraineeRepository;
 import com.fmdgroup.vatcher.repositories.UserRepository;
+import com.fmdgroup.vatcher.services.ITraineeService;
+import com.fmdgroup.vatcher.services.TraineeService;
 
 @Controller
 public class TraineeController {
 
+	
+	@Autowired
+	private ITraineeService service;
+	private TraineeService traineeService;
 	private final TraineeRepository traineeRepository;
+	private UserRepository userRepository;
 
 	public TraineeController(TraineeRepository traineeRepository, UserRepository userRepository) {
 		super();
 		this.traineeRepository = traineeRepository;
 	}
-
+	@RequestMapping(method = RequestMethod.POST, value = "/trainees")
+	
+	public String saveTrainee(@RequestBody Trainee trainee) {
+		traineeService.addTrainee(trainee);
+	        return "addUser" ;
+	    
+	       
+	    }
+	
 	@RequestMapping("/trainees")
 	public String getTrainees(Model model) {
 		model.addAttribute("trainees", traineeRepository.findAll());
 		return "trainee";
 	}
 
-	@RequestMapping("/addQualification")
-	public Set<String> addQualification(Long id, String qualification) throws Exception {
+	@RequestMapping(method = RequestMethod.POST, value = "/addQualification/{id}")
+	
+	public String updateQualification(ModelMap model ,@RequestParam String qualification, @PathVariable Long id)  {
+		Set<String> traineeQualifications = service.traineeQualification();
+		traineeQualifications.add(qualification);
+		model.addAttribute(qualification, traineeQualifications);
+		
+		return "singleUser";
 
-		Optional<Trainee> trainee = traineeRepository.findById(id);
-		if (trainee.isPresent()) {
-			Set<String> traineeQualifications = trainee.get().getQualifications();
-			traineeQualifications.add(qualification);
-			traineeRepository.save(trainee.get());
-
-			return trainee.get().getJobsPreferences();
-		} else {
-			return trainee.get().getJobsPreferences();
-		}
+		
 
 	}
+	
 
 }
