38532ec618cd02393fd69fb6a256eafc6e4e2bc5
FilipDabrowski <fil.dabr@gmail.com>
2023-01-30 18:44:09 +0100

    Update UserRepository.java

--- src/main/java/com/fmdgroup/vatcher/repositories/UserRepository.java
+++ src/main/java/com/fmdgroup/vatcher/repositories/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fmdgroup.vatcher.model.SingleUser;
 
 public interface UserRepository extends JpaRepository<SingleUser, Long> {
+	
 
 }
