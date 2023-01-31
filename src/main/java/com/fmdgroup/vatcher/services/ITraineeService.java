97507e7504e218ca8757379baecdd360419ade88
FilipDabrowski <fil.dabr@gmail.com>
2023-01-31 16:38:25 +0100

    Update ITraineeService.java

--- src/main/java/com/fmdgroup/vatcher/services/ITraineeService.java
+++ src/main/java/com/fmdgroup/vatcher/services/ITraineeService.java
@@ -1,6 +1,7 @@
 package com.fmdgroup.vatcher.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.fmdgroup.vatcher.model.Trainee;
 
 public interface ITraineeService {
-	Set<String> traineeQualification();
+	
+	
+	void updateJobPreferences(String qualification ,Long id) throws Exception;
 	void updateQualification(String qualification ,Long id) throws Exception;
 	Trainee findTraineeById(Long id) throws Exception;
 	String addTrainee(Trainee trainee) throws Exception;
