a8fd4fecfafbb81680686098f02d1a9e9c1eba0b
FilipDabrowski <fil.dabr@gmail.com>
2023-01-31 12:47:47 +0100

    Update TraineeService.java

--- src/main/java/com/fmdgroup/vatcher/services/TraineeService.java
+++ src/main/java/com/fmdgroup/vatcher/services/TraineeService.java
@@ -1,29 +1,30 @@
 package com.fmdgroup.vatcher.services;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+
 import com.fmdgroup.vatcher.model.Trainee;
 import com.fmdgroup.vatcher.repositories.TraineeRepository;
+
 @Service
 public class TraineeService implements ITraineeService {
 
-private TraineeRepository traineeRepo;
+	private TraineeRepository traineeRepo;
 
 	@Override
 	public Set<String> traineeQualification() {
-	
+
 		return null;
 	}
-
-	
 
 	@Override
 	public Trainee findTraineeById(Long id) throws Exception {
 		Optional<Trainee> optTrainee = traineeRepo.findById(id);
-		return optTrainee.orElseThrow(()-> new Exception());
+		return optTrainee.orElseThrow(() -> new Exception());
 	}
 
 	@Override
@@ -32,22 +33,15 @@ private TraineeRepository traineeRepo;
 		Set<String> traineeQualification = trainee.getQualifications();
 		traineeQualification.add(qualification);
 		traineeRepo.save(trainee);
-		
+
 	}
 
-
-
-	
-
-
 	@Override
-	public String addTrainee(Trainee trainee) {
+	public String addTrainee(Trainee trainee) throws Exception {
 		traineeRepo.save(trainee);
 		return trainee.getUser().getName();
 	}
-
-
-
-	
-	
+	public List<Trainee> findAllTrainee() {
+		return traineeRepo.findAll();
+	}
 }
