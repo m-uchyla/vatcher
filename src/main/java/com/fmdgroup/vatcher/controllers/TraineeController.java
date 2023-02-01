95ed95f451c24bec26e52ffdd985d86951ad909c
FilipDabrowski <fil.dabr@gmail.com>
2023-02-01 10:10:10 +0100

    Update TraineeController.java

--- src/main/java/com/fmdgroup/vatcher/controllers/TraineeController.java
+++ src/main/java/com/fmdgroup/vatcher/controllers/TraineeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fmdgroup.vatcher.model.JobOpportunity;
 import com.fmdgroup.vatcher.model.SingleUser;
 import com.fmdgroup.vatcher.model.Trainee;
@@ -28,6 +29,8 @@ public class TraineeController {
 
 	
 	@Autowired
+	ObjectMapper objectMapper;
+	@Autowired
 	private ITraineeService service;
 	private TraineeService traineeService;
 	private final TraineeRepository traineeRepository;
@@ -37,33 +40,58 @@ public class TraineeController {
 		super();
 		this.traineeRepository = traineeRepository;
 	}
-	@RequestMapping(method = RequestMethod.POST, value = "/trainees")
+
+	@RequestMapping(method = RequestMethod.POST, value = "/addtrainee")
+
+	public String addTrainee(ModelMap model, @ModelAttribute Trainee trainee) throws Exception {
+		service.addTrainee(trainee);
+		populateModel(model);
+		return "singleUser";
+
+	}
 	
-	public String saveTrainee(@RequestBody Trainee trainee) {
-		traineeService.addTrainee(trainee);
-	        return "addUser" ;
-	    
-	       
-	    }
+	@RequestMapping(method = RequestMethod.POST, value = "/deletetrainee")
+
+	public String deleteTrainee(ModelMap model, @ModelAttribute Trainee trainee) throws Exception {
+		service.deleteTrainee(trainee.getId());
+		populateModel(model);
+		return "singleUser";
+
+	}
+	
 	
 	@RequestMapping("/trainees")
 	public String getTrainees(Model model) {
 		model.addAttribute("trainees", traineeRepository.findAll());
-		return "trainee";
+		return "singleUser";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/addJobPreferences/{id}")
+	public String updatePreferences(ModelMap model, @RequestParam String jobPreference, @PathVariable Long id)
+			throws Exception {
+
+		service.updateJobPreferences(jobPreference, id);
+		populateModel(model);
+		return "singleUser";
+
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/addQualification/{id}")
-	
-	public String updateQualification(ModelMap model ,@RequestParam String qualification, @PathVariable Long id)  {
-		Set<String> traineeQualifications = service.traineeQualification();
-		traineeQualifications.add(qualification);
-		model.addAttribute(qualification, traineeQualifications);
-		
+
+	public String updateQualification(ModelMap model, @RequestParam String qualification, @PathVariable Long id)
+			throws Exception {
+
+		service.updateQualification(qualification, id);
+		populateModel(model);
 		return "singleUser";
 
-		
-
 	}
+
+	private void populateModel(ModelMap model) {
+		model.addAttribute("trainees", service.findAllTrainee());
+	}
+	
+	
 	
 	// this is for retrieving job offers applied by the trainee user:
 	@RequestMapping("/trainees/{id}/jobOpportunities")							
@@ -76,6 +104,8 @@ public class TraineeController {
 	    return "error";
 	  }
 	}
+	
+
 	
 
 }
